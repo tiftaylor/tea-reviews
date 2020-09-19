ca178310770367a27f0922c0818697aa210295c6
Tif Taylor <tiftaylorisrad@gmail.com>
2020-09-19 11:06:19 -0700

    Added | addClass activate text upon scroll

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
     
+    // activate text transition
+    function isTextInView(element, fullView) {
+        const wFromTop = $(window).scrollTop()
+        const wFromBot = wFromTop + $(window).height()
+        const eOffsetTop = $(element).offset().top
+        const eOffsetBot = eOffsetTop + $(element).height();
+
+        if (fullView) {
+            return wFromTop < eOffsetTop && eOffsetBot < wFromBot;
+        } else {
+            return eOffsetTop <= wFromBot && wFromTop <= eOffsetBot;
+        }
+    }
+
+    const textFade = $(".text-fade");
+    function activateText() {
+        textFade.each(function() {
+            var element = $(this);
+            if (isTextInView(element, false)) {
+                element.addClass("activate");
+            }
+        });
+    }
+
+    $(document).scroll(function() {
+        activateText();
+    });
+
+
     // card slide deck
     $('#slick').slick({
         infinite: false,
@@ -19,8 +48,10 @@ $(document).ready(function(){
         }
 
         reader.readAsDataURL(this.files[0]);
-    })
+    });
 
 
+    // general calls
+    activateText();
 });
 
